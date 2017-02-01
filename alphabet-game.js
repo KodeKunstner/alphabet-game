0a2eb4bcbdd23b7169af37da4a18f9c276118603
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-02-01 12:37:35 +0100

    Commit alphabet-game.js via https://appedit.solsort.com/?Edit/js/gh/KodeKunstner/alphabet-game.

--- alphabet-game.js
+++ alphabet-game.js
@@ -2,12 +2,13 @@
 //
 // This is a bit of documentation, try 'Read' above. Code can be written as semi-literate code, see more here <https://en.wikipedia.org/wiki/Literate_programming>
 
-exports.meta = {
-  id: 'alphabet-game',
-  version: '0.0.1',
-  npm: {}
-};
+exports.meta = { id: 'alphabet-game' };
 var ss = require('solsort@0.1');
+var letters = 'abcdefghijklmnopqrstuvwxyz';
+var letter = letters[(Math.random() * letters.length) | 0];
+ss.handle('letter', o => console.log('letter', o));
+
+function ui() {
 ss.html(`
 <style>
 #alphabet {
@@ -18,10 +19,17 @@ background: red;
 }
 </style>
 <div id=alphabet
+onclick=${ss.htmlEvent('letter')}
 >
+
+<h1>${letter}</h1>
+<input autofocus onkeydown=${ss.htmlEvent('letter')}>
 ${123}
-{ss.htmlEvent('hello')}
+
 "Hello world"
 </div>
-<center>
 `);
+}
+
+ui();
+  
