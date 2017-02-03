23809b51ecb186f9d193f4216d52f6b8fedcd280
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-02-03 17:49:02 +0100

    Commit alphabet-game.js via https://appedit.solsort.com/?Edit/js/gh/KodeKunstner/alphabet-game.

--- alphabet-game.js
+++ alphabet-game.js
@@ -1,40 +1,35 @@
-// # alfabet spil til Theodor
+// # Sample app 
 //
 // This is a bit of documentation, try 'Read' above. Code can be written as semi-literate code, see more here <https://en.wikipedia.org/wiki/Literate_programming>
 
-exports.meta = { id: 'alphabet-game' };
+exports.meta = {
+  id: 'alphabet-game',
+  version: '0.0.1'
+};
 var ss = require('solsort@0.1');
 var da = require('direape@0.1');
 
-var letters = 'abcdefghijklmnopqrstuvwxyz';
-function randomLetter() {
-  return letters[(Math.random() * letters.length) | 0];
-}
-da.setJS(['letter'], da.getJS(['letter'], randomLetter()));
-ss.handle('letter', o => {
-  console.log('letter', o);
-  da.setJS(['letter'], randomLetter());
-  ui();
+da.handle('key',  o => {
+  var key = String.fromCharCode(o.keyCode).toUpperCase();
+  console.log(key);
 });
-
-function ui() {
-ss.html(`
-<style>
-#alphabet {
-display: inline-block;
-width: 100%;
-height: 100%;
-background: red;
-}
-</style>
-<div id=alphabet
->
-
-<h1>${da.getJS(['letter'])}</h1>
-<input autofocus onkeydown=${ss.htmlEvent('letter')}>
-</div>
-`);
+var letters = '';
+for(var i = 0; i < 40*10; i++) {
+  letters += String.fromCharCode(65 + Math.random() * 25 | 0);
+  letters += ' ';
 }
 
-ui();
-  
+
+ss.html(['div',
+         ['code',
+          {style: {fontSize: 20,
+                   display: 'inline-block',
+                   width: '44ex'
+                  }}, letters],
+         ['br'],
+  ['input', {
+    value: '',
+  autoFocus: true,
+  onKeyDown: ss.event('key', {extract:'keyCode'})
+}]]); 
+console.log(da);
